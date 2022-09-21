a61e73d1b269a2a609bca56caaf725a6c9041643
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2022-09-21 11:22:09 +0000

    add edit to lives

--- run.py
+++ run.py
@@ -106,6 +106,7 @@ def begin_game(word):
         elif guess in guessed_letters:
             print(f"\n{Color.YELLOW}You have already  used me"
                   f" {Color.RESET}{guess}{Color.YELLOW}!{Color.RESET}")
+            lives -= 1      
         elif guess not in word:
             print(f"\n{Color.YELLOW}Oh no! {Color.RESET}{guess}"
                   f"{Color.YELLOW} isn't in the word!\n{Color.RESET}")
